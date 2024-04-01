a44e95f148c847bc7ec1818d49d1216e8e9d4b80
ahmadthufail <ahmadthufail.student@unibamadura.ac.id>
2024-04-02 03:56:37 +0700

    tugas php 4

---
+++ home.php
@@ -0,0 +1,22 @@
+<div class="container pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 pt-5">
+                <div class="box text-box">
+                    <h2>MY furniture</h2>
+                    <p>Kami adalah tujuan utama Anda untuk memenuhi kebutuhan furnitur berkualitas tinggi dengan harga yang terjangkau. Dengan beragam pilihan kursi, meja, lemari, dan aksesori lainnya, kami berkomitmen untuk memberikan pengalaman berbelanja yang memuaskan bagi Anda.</p>
+                    <p>Dengan desain yang elegan dan material berkualitas, setiap produk furnitur kami dibuat untuk memberikan kenyamanan dan gaya yang tak tertandingi untuk rumah atau ruangan Anda. Dari gaya klasik hingga modern, kami memiliki sesuatu untuk setiap selera dan ruang.</p>
+                    <p>Tim kami yang berpengalaman siap membantu Anda dalam memilih furnitur yang tepat sesuai dengan kebutuhan dan preferensi Anda. Kami percaya bahwa setiap rumah adalah cerminan dari kepribadian dan gaya hidup pemiliknya, dan kami berkomitmen untuk membantu Anda menciptakan ruang yang indah dan berfungsional.</p>
+                    <p>Selamat berbelanja dan jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau membutuhkan bantuan. Terima kasih telah memilih kami sebagai mitra furnitur Anda!</p>
+  
+                </div>
+            </div>
+
+            <div class="col-md-6 text-center"  style="background-image: url('/img/home.jpg'); background-size: cover; background-position: center;">
+                <div class="box image-box">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
